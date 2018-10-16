10b27c4df595aa3a428216d2d34ffa13ca61254d
Janne Leppänen <janne@janneleppanen.com>
2018-10-16 20:30:14 +0300

    Store state to localstorage

--- src/config/store.ts
+++ src/config/store.ts
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "../redux";
 import createSagaMiddleware from "redux-saga";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import sagas from "../sagas";
 
@@ -9,12 +11,18 @@ const sagaMiddleware = createSagaMiddleware();
 const enhancer =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  reducers,
+const persistConfig = {
+  key: "root",
+  storage
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = createStore(
+  persistedReducer,
   {},
   enhancer(applyMiddleware(sagaMiddleware))
 );
 
-sagaMiddleware.run(sagas);
+export const persistor = persistStore(store);
 
-export default store;
+sagaMiddleware.run(sagas);
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,21 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 // import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { theme } from "./config/global-styles";
-import store from "./config/store";
+import { store, persistor } from "./config/store";
 import Routes from "./config/routes";
 
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <Routes />
-    </ThemeProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </PersistGate>
   </Provider>,
   document.getElementById("root") as HTMLElement
 );
