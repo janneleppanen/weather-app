4208976c8d33e82c8287bb61cf170aaa1a50c0c0
Janne Leppänen <janne@janneleppanen.com>
2018-11-09 18:46:21 +0200

    Add some tests

--- src/services/OpenWeatherMap.ts
+++ src/services/OpenWeatherMap.ts
@@ -1,6 +1,6 @@
 const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
-const FORECAST_BASE_URL = `//api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&`;
-const FIND_BASE_URL = `//api.openweathermap.org/data/2.5/find?appid=${API_KEY}&`;
+const FORECAST_BASE_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&`;
+const FIND_BASE_URL = `https://api.openweathermap.org/data/2.5/find?appid=${API_KEY}&`;
 
 export const getForecast = async (location: string) => {
   const res = await fetch(`${FORECAST_BASE_URL}q=${location}&mode=json`);
