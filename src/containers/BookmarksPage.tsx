2ff7ffc844dcf301a23c0442d04d0a47bb51bab1
Janne Leppänen <janne@janneleppanen.com>
2018-11-10 13:36:04 +0200

    Use redux compose instead of lodash/fp compose

--- src/containers/BookmarksPage.tsx
+++ src/containers/BookmarksPage.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { connect } from "react-redux";
+import { compose } from "redux";
 import { withNamespaces } from "react-i18next";
-import { compose } from "lodash/fp";
 import { Link } from "react-router-dom";
 
 import BookmarkLink from "../components/BookmarkLink";
