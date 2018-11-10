e4f567679972da0101a0fb401549d4abb15d85fd
Janne Leppänen <janne@janneleppanen.com>
2018-11-11 00:04:01 +0200

    Fix SearchField test

--- src/__tests__/components/SearchField.test.tsx
+++ src/__tests__/components/SearchField.test.tsx
@@ -5,5 +5,5 @@ import SearchField from "../../components/SearchField";
 
 it("renders button", () => {
   const wrapper = enzyme.shallow(<SearchField value="inputvalue" />);
-  expect(wrapper.props().value).toBe("inputvalue");
+  expect(wrapper.props().children[0].props.value).toBe("inputvalue");
 });
