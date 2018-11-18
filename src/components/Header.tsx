1aa6896d8b69a8f563095094328566e91e4623c5
Janne Leppänen <janne@janneleppanen.com>
2018-11-18 17:19:29 +0200

    Refactor header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,52 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { withNamespaces } from "react-i18next";
+
+import { ReactComponent as BookmarksSVG } from "../images/icons/star.svg";
+import { ReactComponent as SettingsSVG } from "../images/icons/settings.svg";
+import { Container } from "../common";
+
+const iconStyles = css`
+  width: 1.25rem;
+  height: 1.25rem;
+  fill: currentColor;
+  transition: all 0.1s;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+const MenuWrapper = styled.header`
+  background-color: ${props => props.theme.headerBackground};
+`;
 
 const Menu = styled.ul`
   list-style: none;
   margin: 0;
-  padding: 0;
+  padding: 0 0.5rem;
   display: flex;
   justify-content: flex-end;
-  background-color: ${props => props.theme.headerBackground};
   color: white;
 `;
 
 const MenuItem = styled.li`
-  padding: 0.5rem;
+  padding: 1rem 0.75rem;
   font-size: 0.875rem;
 `;
 
 const MenuLink = styled(Link)`
   color: ${props => props.theme.headerText};
+  display: flex;
+  align-items: center;
+`;
+
+const BookmarkIcon = styled(BookmarksSVG)`
+  ${iconStyles}
+`;
+
+const SettingsIcon = styled(SettingsSVG)`
+  ${iconStyles}
 `;
 
 interface Props {
@@ -27,19 +54,24 @@ interface Props {
 }
 
 const Header = (props: Props) => {
-  const { t } = props;
+  // const { t } = props;
   return (
-    <Menu>
-      <MenuItem>
-        <MenuLink to="/">{t("navigation.home")}</MenuLink>
-      </MenuItem>
-      <MenuItem>
-        <MenuLink to="/bookmarks">{t("navigation.locations")}</MenuLink>
-      </MenuItem>
-      <MenuItem>
-        <MenuLink to="/settings">{t("navigation.settings")}</MenuLink>
-      </MenuItem>
-    </Menu>
+    <MenuWrapper>
+      <Container>
+        <Menu>
+          <MenuItem>
+            <MenuLink to="/bookmarks">
+              <BookmarkIcon />
+            </MenuLink>
+          </MenuItem>
+          <MenuItem>
+            <MenuLink to="/settings">
+              <SettingsIcon />
+            </MenuLink>
+          </MenuItem>
+        </Menu>
+      </Container>
+    </MenuWrapper>
   );
 };
 
